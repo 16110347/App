ca00e12f132152b1a06fc75edec880cae90cb029
16110347 <afdhal.luthfi@students.amikom.ac.id>
2018-12-09 16:08:35 +0700

    formDaftar

--- php/logic.php
+++ php/logic.php
@@ -1,7 +1,27 @@
 <?php 
 
-echo "Hallo apa kaabr"
+$conn = mysqli_connect("localhost","root","","E-vent");
 
+//var_dump($conn);
+
+function kirim($data){
+    global $conn;
+    $nama = $data['nama'];
+    $email = $data['email'];
+    $password = $data['password'];
+    $alamat = $data['alamat_1'];
+    $alamat2 = $data['alamat_2'];
+    $negara = $data['negara'];
+    $kota = $data['kota'];
+    $kodePos = $data['kode_pos'];
+
+    $query = "INSERT INTO User_Login
+                VALUES
+                ('','$nama','$email','$password','$alamat','$alamat2','$kota','$negara','$kodePos')";
+
+    mysqli_query($conn,$query);
+    return mysqli_affected_rows($conn);
+};
 
 
 ?>
