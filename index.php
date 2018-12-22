e19d077dddfce5f64aa2a1f3f17b1bb231d71822
16110347 <afdhal.luthfi@students.amikom.ac.id>
2018-12-22 09:57:42 +0700

    tambah

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <style type="text/css">
+    	
+    *{
+    	border: 1px solid black;
+    }
+
+
+
+
+    </style>
+  </head>
+  <body>
+
+
+
+  	<div class="container">
+  		
+  		<div class="row">
+  			
+  		<div class="col-sm-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel repellendus sed quas repudiandae consectetur! Modi a repellat, voluptas voluptatem facere consectetur eligendi, quas veniam aliquid provident est magnam amet eum itaque mollitia. Earum voluptas itaque porro dolorum! Incidunt nisi consequatur nemo corporis repellendus asperiores sint reprehenderit voluptatum qui, atque ex quaerat saepe sapiente assumenda ipsam, quasi nostrum excepturi perferendis quidem. Nulla ex nam commodi vero, vel, a officiis dolores, dolorem tenetur, voluptatem facilis eius dicta. Nemo animi commodi necessitatibus aut nesciunt ratione expedita officiis ullam vel nihil pariatur quae molestiae veniam iusto culpa, incidunt. Necessitatibus, aliquam sunt aliquid. Iusto laboriosam laborum, tempore, aut id veniam labore corrupti sequi dignissimos voluptatibus earum ipsum dolores sit. Perferendis eos at vitae doloremque? Eos quis deserunt dignissimos at nobis possimus alias, porro aliquid assumenda facilis, tempore, rem temporibus minus cum, nostrum laborum ratione veniam nam recusandae. Culpa minima magni ipsam incidunt ab magnam nostrum laboriosam eligendi voluptatum cumque aspernatur accusantium dolores, facilis doloribus laudantium iure officiis tenetur modi est placeat. Illo quisquam, dolores culpa placeat tenetur maxime nostrum laborum libero fugit dolorum iste quae unde quia magni dicta ratione voluptatibus quibusdam omnis veniam sint eligendi minima doloribus asperiores? In accusamus odit laborum, tempora sed, vitae sapiente unde iusto eaque suscipit cupiditate nobis ex commodi, ducimus tempore repellat. Minus consequuntur exercitationem molestiae qui libero vitae nesciunt architecto veniam hic enim, asperiores adipisci id, dolor cumque eaque repellat ut! Soluta quisquam quos obcaecati. Aliquid nisi voluptatem perspiciatis. Veniam aspernatur, tenetur eligendi optio.</p>
+      </div>
+  		<div class="col-sm-8">
+      
+      
+      
+      </div>
+
+
+  		</div>
+
+
+  	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  </body>
+</html>
